2a8d69027ab65a720974ca77349fa1770ccd9380
unknown <ahxpg>
2020-03-08 20:02:53 +0100

    update data 08.03.2020 8pm

--- assets/google/google_tablechart.js
+++ assets/google/google_tablechart.js
@@ -2,8 +2,15 @@ google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
 
 function drawTable() {
+	
+  var jsonData = $.ajax({
+    url: "https://raw.githubusercontent.com/haruabba/corona-aktuell/master/germany_dataset.json",
+    dataType: "json",
+    async: false,
+  }).responseText;
+
   var worldData = google.visualization.arrayToDataTable(worldDataSet);
-  var germanyData = google.visualization.arrayToDataTable(germanyDataSet);
+  var germanyData = new google.visualization.DataTable(jsonData);
 
   var worldTable = new google.visualization.Table(document.getElementById('worldTable_div'));
   var germanyTable = new google.visualization.Table(document.getElementById('germanyTable_div'));
