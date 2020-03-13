c2c70f25e85c44631fa36912657ee91ecc277b27
haruabba <ahxpg>
2020-03-13 13:06:03 +0100

    remove weltkarte

--- assets/google/google_geochart.js
+++ assets/google/google_geochart.js
@@ -12,14 +12,7 @@ function drawMarkersMap() {
     async: false,
   }).responseText;
 
-  var worldMap = google.visualization.arrayToDataTable(worldDataSet);
   var germanyMap = new google.visualization.DataTable(jsonData);
-  //var germanyMap = new google.visualization.arrayToDataTable(germanyDataSet);
-
-   var worldOptions = {
-    colorAxis: {minValue: 0, maxValue: 1000, colors: ['#FFE4E1', '#A52A2A']},
-    legend: 'none'
-   };
 
    var germanyOptions = {
     displayMode: 'regions',
@@ -29,8 +22,6 @@ function drawMarkersMap() {
     legend: 'none'
   };
 
-  var worldChart = new google.visualization.GeoChart(document.getElementById('welt_div'));
   var countryChart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-  worldChart.draw(worldMap, worldOptions);
   countryChart.draw(germanyMap, germanyOptions);
 }
