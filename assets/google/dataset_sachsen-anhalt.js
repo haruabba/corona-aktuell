72227f70845a8e75685455ca4a5120d63de5df04
haruabba <ahxpg>
2020-03-15 21:17:06 +0100

    update studium information /tablechart in sachsen anhalt

--- assets/google/dataset_sachsen-anhalt.js
+++ assets/google/dataset_sachsen-anhalt.js
@@ -1,15 +1,11 @@
     const sachsenanhaltDataSetProDay = [
         ['X', 'Bestätiger', 'Tod'],
         [new Date(2020, 2, 9), 0, 0],    
-        [new Date(2020, 2, 10), 9, 0],   
-    ];
+        [new Date(2020, 2, 10), 8, 0],   
+        [new Date(2020, 2, 11), 14, 0],   
+        [new Date(2020, 2, 12), 24, 0],   
+        [new Date(2020, 2, 13), 33, 0],   
+        [new Date(2020, 2, 14), 44, 0],   
+        [new Date(2020, 2, 15), 54, 0],    
 
-  const sachsenanhaltStadt = [
-    ['Stadt', 'Bestätiger', 'Tod'],
-    [{v: 'Halle(Saale)', f: 'Halle'}, 3, 0],
-    [{v: 'Magdeburg', f: 'Magdeburg'}, 1, 0],
-    [{v: 'Burgenlandkreis', f: 'Burgenlandkreis'}, 1, 0],
-    [{v: 'Landkreis Börde', f: 'Börde'}, 2, 0],
-    [{v: 'Salzlandkreis', f: 'Salzlandkreis'}, 1, 0],
-    [{v: 'Saalekreis', f: 'Saalekreis'}, 1, 0],
-  ];    
+    ];
