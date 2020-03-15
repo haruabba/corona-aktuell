647b6942ba06a68b47b5612d9d15b818668bd3e1
haruabba <ahxpg>
2020-03-15 01:47:33 +0100

    fix json syntax error

--- assets/google/google_tablechart.js
+++ assets/google/google_tablechart.js
@@ -9,12 +9,8 @@ function drawTable() {
     async: false,
   }).responseText;
 
-  var worldData = google.visualization.arrayToDataTable(worldDataSet);
   var germanyData = new google.visualization.DataTable(jsonData);
-
-  var worldTable = new google.visualization.Table(document.getElementById('worldTable_div'));
   var germanyTable = new google.visualization.Table(document.getElementById('germanyTable_div'));
 
-  worldTable.draw(worldData, {showRowNumber: true});
   germanyTable.draw(germanyData, {showRowNumber: true});
 }
