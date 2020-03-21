177a1426e17bb32a776e8a9552e0ab7e271bbcd4
haruabba <ahxpg>
2020-03-21 02:16:06 +0100

    add linechart/tablechart for Thüringen

---
+++ assets/google/dataset_thueringen.js
@@ -0,0 +1,15 @@
+    const thueringenDataSetProDay = [
+        ['X', 'Bestätiger', 'Tod'],
+        [new Date(2020, 2, 9), 0, 0],    
+        [new Date(2020, 2, 10), 8, 0],   
+        [new Date(2020, 2, 11), 14, 0],   
+        [new Date(2020, 2, 12), 24, 0],   
+        [new Date(2020, 2, 13), 33, 0],   
+        [new Date(2020, 2, 14), 44, 0],   
+        [new Date(2020, 2, 15), 54, 0],    
+        [new Date(2020, 2, 16), 77, 0],
+        [new Date(2020, 2, 17), 105, 0],
+        [new Date(2020, 2, 18), 133, 0],
+        [new Date(2020, 2, 19), 174, 0],
+
+    ];
