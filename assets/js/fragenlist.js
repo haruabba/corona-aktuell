6585e215eb6833ec97286361c65e6e60b0d1e73e
haruabba <ahxpg>
2020-03-22 23:32:01 +0100

    remove semicolons

--- assets/js/fragenlist.js
+++ assets/js/fragenlist.js
@@ -4,21 +4,10 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;  
-
-    
-    if (panel.style.maxHeight){
-      panel.previousElementSibling.lastElementChild.lastElementChild.firstElementChild.className = "plus vertical-line";
-      panel.style.maxHeight = null;
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = "0px";
     } else {
-              for(var it of acc) {
-                it.lastElementChild.lastElementChild.firstElementChild.className = "plus vertical-line";
-        it.classList.remove("active");
-        it.nextElementSibling.style.maxHeight = null;
-        
-    
-        }
-      panel.previousElementSibling.lastElementChild.lastElementChild.firstElementChild.className = "plus";
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   });
