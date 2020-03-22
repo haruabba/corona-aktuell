fa47386bb5bfb0d11b60de11ff1ff9e4a4dd5776
haruabba <ahxpg>
2020-03-22 22:40:57 +0100

    fix accordian bug in js

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,18 +1,19 @@
+function accordion() {
+  /* Toggle between adding and removing the "active" class,
+  to highlight the button that controls the panel */
+  this.classList.toggle("active");
+
+  /* Toggle between hiding and showing the active panel */
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
+}
+
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-   acc[i].addEventListener("click", function accordion() {
-     // Toggle between adding and removing the "active" class,
-     // to highlight the button that controls the panel
-     this.classList.toggle("active");
-
-     // Toggle between hiding and showing the active panel */
-     var panel = this.nextElementSibling;
-     if (panel.style.display === "block") {
-       panel.style.display = "none";
-     } else {
-       panel.style.display = "block";
-     }
-   });
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", accordion);
 }
